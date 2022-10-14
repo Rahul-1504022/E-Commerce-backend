67af798db0fb29a7035f3a94e44fa2b32a876c08
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-10-14 22:20:44 +0600

    server setup

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv/config');
 const mongoose = require('mongoose');
 const app = require('./app');
+const DB = process.env.ATLAS_URL_SERVER.replace('<PASSWORD>', process.env.ATLAS_PASS);
 
 global.__basedir = __dirname;
 
 //SERVER connection
-mongoose.connect(process.env.MONGODB_URL_LOCAL)
+mongoose.connect(DB)
     .then(() => console.log("Connected to MongoDB!"))
     .catch((err) => console.log("Connection Failed!"));
 //PORT initialization
