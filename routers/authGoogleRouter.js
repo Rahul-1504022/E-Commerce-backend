4005a9d136d0c3ac9658391f6423ebac2e778559
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-11-14 21:18:50 +0600

    demo

--- routers/authGoogleRouter.js
+++ routers/authGoogleRouter.js
@@ -9,7 +9,8 @@ router.route('/')
 //  (When google redirect)
 router.route('/redirect')
     .get(passport.authenticate("google", { session: false }), (req, res) => {
-        res.status(200).send(req.user); //user is a by default attribute
+        // res.status(200).send(req.user); //user is a by default attribute
+        res.sendFile(path.join(__basedir, "public/loginSuccess.html"));
     })
 
 module.exports = router;
