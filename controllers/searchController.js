9cb4e739f1655bfcd7896b91f6ba991b177bba9a
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-11-23 22:13:52 +0600

    demo

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,5 +3,8 @@ const { Product } = require('../models/product');
 module.exports.searchResult = async (req, res) => {
     const search = req.params.search;
     const result = await Product.find({ name: { $regex: `/${search}/i` } })
-    return res.status(200).send(result);
+    return res.status(200).send({
+        result: result,
+        search: search,
+    });
 }
